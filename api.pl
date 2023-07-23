18abf27ad7e963e06055fbf4eb0f661704d783c5
mirovsky <mirovsky@a32e68db-fe4c-4835-81c6-61a328cef007>
2023-07-23 06:23:36 +0000

    Some documentation nodes to setting redirection in Appache2
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/signal_a_sum/source_detection/Perl_UDPipeREST@49 a32e68db-fe4c-4835-81c6-61a328cef007

--- api.pl
+++ api.pl
@@ -1,5 +1,31 @@
 #!/usr/bin/env perl
 
+=item
+# skript se na serveru spustí pomocí
+morbo api.pl
+
+# Pak naslouchá na defaultním portu 3000 a lokálně funguje např.:
+curl http://localhost:3000/api/test
+
+# Perlovský balíček Mojolicious obsahující i příkaz morbo se instaloval pomocí 
+sudo apt-get install libmojolicious-perl
+
+# Pro přesměrování požadavků z Apache2 bylo mj. potřeba nastavit v /etc/apache2/sites-available/000-default.conf v sekci <VirtualHost *:80>:
+        ServerName localhost
+	# Proxy pro /api/detect a /api/test
+        ProxyPass "/api/detect" "http://localhost:3000/api/detect"
+        ProxyPassReverse "/api/detect" "http://localhost:3000/api/detect"
+        ProxyPass "/api/test" "http://localhost:3000/api/test"
+        ProxyPassReverse "/api/test" "http://localhost:3000/api/test"
+# A v /etc/apache2/apache2.conf bylo potřeba přidat:
+	LoadModule proxy_module modules/mod_proxy.so
+	LoadModule proxy_http_module modules/mod_proxy_http.so
+# Pak funguje např.
+curl http://localhost/api/test
+
+=cut
+
+
 use Mojolicious::Lite;
 
 # Endpoint pro test
