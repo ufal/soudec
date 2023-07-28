e8f43cf993bc96c580c2efd1bc109eed6a44b014
mirovsky <mirovsky@a32e68db-fe4c-4835-81c6-61a328cef007>
2023-07-28 12:23:51 +0000

    A dummy result response
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/signal_a_sum/source_detection/Perl_UDPipeREST@58 a32e68db-fe4c-4835-81c6-61a328cef007

--- system/api.pl
+++ system/api.pl
@@ -1,43 +1,62 @@
 #!/usr/bin/env perl
 
-=item
 # skript se na serveru spustí pomocí
-morbo api.pl
+# morbo api.pl
 
 # Pak naslouchá na defaultním portu 3000 a lokálně funguje např.:
-curl http://localhost:3000/api/test
+# curl http://localhost:3000/api/test
 
 # Perlovský balíček Mojolicious obsahující i příkaz morbo se instaloval pomocí 
-sudo apt-get install libmojolicious-perl
+# sudo apt-get install libmojolicious-perl
 
 # Pro přesměrování požadavků z Apache2 bylo mj. potřeba nastavit v /etc/apache2/sites-available/000-default.conf v sekci <VirtualHost *:80>:
-        ServerName localhost
-	# Proxy pro /api/detect a /api/test
-        ProxyPass "/api/detect" "http://localhost:3000/api/detect"
-        ProxyPassReverse "/api/detect" "http://localhost:3000/api/detect"
-        ProxyPass "/api/test" "http://localhost:3000/api/test"
-        ProxyPassReverse "/api/test" "http://localhost:3000/api/test"
+#        ServerName localhost
+#        # Proxy pro /api/detect a /api/test
+#        ProxyPass "/api/detect" "http://localhost:3000/api/detect"
+#        ProxyPassReverse "/api/detect" "http://localhost:3000/api/detect"
+#        ProxyPass "/api/test" "http://localhost:3000/api/test"
+#        ProxyPassReverse "/api/test" "http://localhost:3000/api/test"
 # A v /etc/apache2/apache2.conf bylo potřeba přidat:
-	LoadModule proxy_module modules/mod_proxy.so
-	LoadModule proxy_http_module modules/mod_proxy_http.so
+#        LoadModule proxy_module modules/mod_proxy.so
+#        LoadModule proxy_http_module modules/mod_proxy_http.so
 # Pak funguje např.
-curl http://localhost/api/test
-
-=cut
+# curl http://localhost/api/test
 
 
 use Mojolicious::Lite;
 
 # Endpoint pro test
-get '/api/test' => sub {
+any '/api/test' => sub {
     my $c = shift;
-    $c->render(json => { message => 'This is the test function.' });
+    if ($c->req->method eq 'POST') {
+        # Zde můžete zpracovat data odeslaná v těle požadavku POST
+        my $data = $c->req->json;
+        # Pro ilustraci jen odeslat data zpět jako odpověď
+        return $c->render(json => { message => 'This is the test function called via POST.',
+                                    result => 'A dummy result' });
+    }
+    else {
+        # Zpracování GET požadavku stejně jako dříve
+        return $c->render(json => { message => 'This is the test function called via GET.',
+                                    result => 'A dummy result' });
+    }
 };
 
 # Endpoint pro detect
-get '/api/detect' => sub {
+any '/api/process' => sub {
     my $c = shift;
-    $c->render(json => { message => 'This is the detect function.' });
+    if ($c->req->method eq 'POST') {
+        # Zde můžete zpracovat data odeslaná v těle požadavku POST
+        my $data = $c->req->json;
+        # Pro ilustraci jen odeslat data zpět jako odpověď
+        return $c->render(json => { message => 'This is the process function called via POST.',
+                                    result => 'A dummy result' });
+    }
+    else {
+        # Zpracování GET požadavku stejně jako dříve
+        return $c->render(json => { message => 'This is the process function called via GET.',
+                                    result => 'A dummy result' });
+    }
 };
 
 app->start;
