e605620b85a957a5ec79ee2762fa428079160fa2
mirovsky <mirovsky@a32e68db-fe4c-4835-81c6-61a328cef007>
2023-08-16 07:33:49 +0000

    A comment describing the process of demonizing api.pl
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/signal_a_sum/source_detection/Perl_UDPipeREST@85 a32e68db-fe4c-4835-81c6-61a328cef007

--- system/run_api.sh
+++ system/run_api.sh
@@ -1,2 +1,61 @@
 morbo api.pl
 
+exit
+
+<<COMMENT
+
+Svoji službu REST API implementovanou v perlu pomocí knihovny Mojolicious::Lite spouštím na serveru pomocí příkazu morbo api.pl. Potřeboval bych, aby ta služba fungovala nezávisle na terminálu a pokud možno i po restartu počítače. Jak se toto řeší?
+
+
+Abyste zajistil, že vaše REST API služba implementovaná v Perl s knihovnou Mojolicious::Lite bude fungovat nezávisle na terminálu a i po restartu počítače, můžete využít několik přístupů. Jedním z možných řešení je použití procesního správce (process manager) a služby (service) pro správu běhu vaší aplikace.
+
+Zde je návod, jak postupovat:
+
+1. **Přesunout kód API do samostatného souboru:**
+   Nejprve přesuňte kód vašeho REST API implementovaného pomocí Mojolicious::Lite do samostatného souboru, například `api.pl`. Ujistěte se, že váš kód obsahuje všechny potřebné moduly a nastavení.
+
+2. **Instalovat službu pro správu procesů:**
+   Na systému, kde chcete spouštět svoji službu, by měl být k dispozici nějaký procesní správce. Pro unixové systémy je jednou z běžných možností [Systemd](https://www.freedesktop.org/wiki/Software/systemd/), který umožňuje definovat a spravovat systémové služby.
+
+3. **Vytvoření konfiguračního souboru pro službu:**
+   Vytvořte konfigurační soubor pro vaši službu. Pro Systemd může vypadat nějak takto (`/etc/systemd/system/my-api.service`):
+
+   ```
+   [Unit]
+   Description=My API Service
+
+   [Service]
+   ExecStart=/usr/bin/morbo /cesta/k/tvemu/api.pl
+   WorkingDirectory=/cesta/k/tvemu
+   Restart=always
+   User=tvoje-uzivatelske-jmeno
+
+   [Install]
+   WantedBy=multi-user.target
+   ```
+
+   Upravte cestu k `api.pl`, pracovní adresář, uživatelské jméno a další parametry podle vašeho nastavení.
+
+4. **Aktivace a spuštění služby:**
+   Po vytvoření konfigurace spusťte následující příkazy:
+
+   ```
+   sudo systemctl daemon-reload
+   sudo systemctl enable my-api
+   sudo systemctl start my-api
+   ```
+
+   Tímto se služba začne automaticky spouštět při startu systému a bude se také automaticky restartovat v případě selhání.
+
+5. **Správa služby:**
+   Službu lze spravovat pomocí příkazů `systemctl`, např.:
+
+   ```
+   sudo systemctl status my-api
+   sudo systemctl stop my-api
+   sudo systemctl restart my-api
+   ```
+
+Tímto způsobem byste měl mít svoji REST API službu spuštěnou jako systémovou službu, která bude fungovat nezávisle na terminálu a bude se také automaticky restartovat po restartu počítače.
+
+COMMENT
